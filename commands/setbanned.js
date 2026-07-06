7411224e80139f854dd158601e308024c8f191a2
TRAFALGAR MD <alphasenykoivogui@gmail.com>
2026-07-06 23:44:12 +0000

    Add files via upload

---
+++ commands/setbanned.js
@@ -0,0 +1,24 @@
+const { setBanned } = require("../lib/messages");
+
+module.exports = async (bot, msg) => {
+
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+
+    const text = msg.text.replace("/setbanned", "").trim();
+
+    if (!text) {
+        return bot.sendMessage(
+            chatId,
+            "❌ Utilisation :\n\n/setbanned Votre message"
+        );
+    }
+
+    setBanned(userId, text);
+
+    bot.sendMessage(
+        chatId,
+`✅ Votre message BANNED a été enregistré avec succès.`
+    );
+
+};
---
+++ commands/setclean.js
@@ -0,0 +1,24 @@
+const { setClean } = require("../lib/messages");
+
+module.exports = async (bot, msg) => {
+
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+
+    const text = msg.text.replace("/setclean", "").trim();
+
+    if (!text) {
+        return bot.sendMessage(
+            chatId,
+            "❌ Utilisation :\n\n/setclean Votre message"
+        );
+    }
+
+    setClean(userId, text);
+
+    bot.sendMessage(
+        chatId,
+`✅ Votre message de compte actif a été enregistré avec succès.`
+    );
+
+};
